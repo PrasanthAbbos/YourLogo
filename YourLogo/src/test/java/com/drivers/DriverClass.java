efa3aff39ae25de918672dc4a2884272789eae38
PrasanthAbbos <10prasanth1994@gmail.com>
2021-03-13 11:26:01 +0530

    intial Project Push

---
+++ YourLogo/src/test/java/com/drivers/DriverClass.java
@@ -0,0 +1,15 @@
+package com.drivers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverClass {
+	
+	public WebDriver launchDriver() {
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+}
---
+++ YourLogo/src/test/java/com/utils/ListenerTest.java
@@ -0,0 +1,44 @@
+package com.utils;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenerTest implements ITestListener{
+	
+	public void onTestStart(ITestResult result) {
+	  
+	  }
+
+	 public  void onTestSuccess(ITestResult result) {
+	    // not implemented
+		 System.out.println("Test Pass");
+	  }
+
+	  public void onTestFailure(ITestResult result) {
+	    // not implemented
+	  }
+
+	  public void onTestSkipped(ITestResult result) {
+	    // not implemented
+	  }
+
+	 
+	  public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+	    // not implemented
+	  }
+
+	
+	  public void onTestFailedWithTimeout(ITestResult result) {
+	    onTestFailure(result);
+	  }
+
+	 
+	  public void onStart(ITestContext context) {
+	    // not implemented
+	  }
+
+	  public void onFinish(ITestContext context) {
+	    // not implemented
+	  }
+}
